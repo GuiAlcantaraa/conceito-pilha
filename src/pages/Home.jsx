a44e04136ea8c83dc8ef63b13d741f4fc7d26b06
Guilherme de Alcantara Machado <guilherme.machado@zema.com>
2022-08-29 17:04:15 -0300

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,59 +1,93 @@
-import { useState } from 'react';
+
 import './styles.scss';
 
 
 
 export function Home() {
 
+    var pilha = []
 
-    const [numero, setNumero] = useState()
+    function adicionarNaPilha() {
 
-    const [stack, setStack] = useState([])
+        if (pilha.length < 5) {
 
-    function adicionarNaFila() {
-        const number = Number(numero)
-        setStack(stack => [...stack, number])
-        console.log(stack)
+            let numeroAleatorio = Math.floor(Math.random() * 50 + 1)
+            pilha.push(numeroAleatorio)
+            console.log(pilha)
 
-    }
-
-    function removerDaFila() {
-        console.log(stack)
-
-
-        let fila = stack
-
-        if (fila.length > 0) {
-            fila.pop()
-            console.log(fila)
         }
         else {
-            alert("Fila ja esta vazia")
+            alert("Overflow: A pilha esta cheia")
+        }
+
+    }
+
+    function removerDaPilha() {
+
+        if (pilha.length > 0) {
+            pilha.pop()
+            console.log(pilha)
+        }
+        else {
+            alert("Underflow: A pilha ja esta vazia")
         }
     }
 
+    function listarPilha() {
+        console.log("A pilha esta assim:", pilha)
+    }
 
+    function maiorMenorMediaPilha() {
 
+        if (pilha.length > 0) {
+
+            const maiorValor = pilha.reduce(function (prev, current) {
+                return prev > current ? prev : current;
+            });
+
+            const menorValor = pilha.reduce(function (prev, current) {
+                return prev < current ? prev : current;
+            });
+
+            const media = mediaPilha()
+
+            console.log("O maior elemento da pilha", maiorValor)
+            console.log("O menor elemento da pilha", menorValor)
+            console.log("A media da pilha é:", media)
+        }
+        else {
+            alert("Não existe nenhum elemento na pilha")
+        }
+
+    }
+
+    function mediaPilha() {
+        let i = 0, sum = 0, arrayLen = pilha.length
+
+        while (i < arrayLen) {
+            sum = sum + pilha[i++]
+        }
+
+        return sum / arrayLen;
+    }
     return (
 
         <div className="container">
 
-            {/* <ul>
-                {stack.map((numbers) => <li key={numbers.toString()}>{numbers}</li>)}
-            </ul> */}
-
-            <input
-                className="input"
-                type="number"
-                onChange={(e) => setNumero(e.target.value)}
-            />
-
-            <button type="button" className="button" onClick={adicionarNaFila}>
+            <button type="button" className="button" onClick={adicionarNaPilha}>
                 Adicionar na pilha
             </button>
 
-            <button type="button" className="button" onClick={removerDaFila}>
+            <button type="button" className="button" onClick={removerDaPilha}>
                 Remover da pilha
+            </button>
+
+            <button type="button" className="button" onClick={listarPilha}>
+                Listar pilha
+            </button>
+
+            <button type="button" className="button" onClick={maiorMenorMediaPilha}>
+                Maior, menor e media
             </button>
         </div>
     )
